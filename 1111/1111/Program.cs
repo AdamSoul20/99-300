22afb7004ff74699df4f3d6a68ed802b3751afa8
AdamSoul20 <66073926+AdamSoul20@users.noreply.github.com>
2020-05-28 21:15:38 +0700

    Add files via upload

---
+++ 1111/1111/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111
+{
+    class Program {
+        static void Main(string[] args) 
+        {
+            int z = 0;
+            Console.WriteLine("Введите 1 число диапозона"); int n = Int32.Parse(Console.ReadLine()); 
+            Console.WriteLine("Введите 2 число диапозона"); int m = Int32.Parse(Console.ReadLine()); 
+            bool b;
+            if (n < m)
+            { 
+                for (int i = n; i <= m; i++)
+                {
+                    b = true;
+                    
+                    for (int j = 2; j < i; j++)
+                    { 
+                        if (i % j == 0) 
+                        { 
+                            b = false;
+                            break; 
+                        } 
+                    }
+                    if (b) 
+                    { 
+                        Console.WriteLine(i + " простое число");
+                        z += 1; 
+                    }   
+                } 
+            }
+            Console.WriteLine(z + "простых чисел");
+            Console.ReadKey();   
+        } 
+    }
+}
